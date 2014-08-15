41bb61532f99cf53f8f5b472c66c9b60870dd0ca
deminette <student@tests-iMac-4.local>
2014-08-15 17:09:03 +0700

    save Tutorial1

--- SwiftTutorial1.playground/section-1.swift
+++ SwiftTutorial1.playground/section-1.swift
@@ -4,4 +4,20 @@ let final=70
 var sum=grade+final
 sum++
 
+let tutorialteam=56
+let editorialteam=23
+var totalteam=tutorialteam+editorialteam
+    totalteam+=1
+    
+let priceIntiferred = -19
+let priceIntExplicit:Int = -19
+
+let priceFloatferred = true
+let priceFloatExplicit:Double = -19.99
+
+let onsaleInferred=true
+let onsaleExplicit:Bool=true
+
+let nameInferred="whoopie Cushion"
+let nameExplicit:String="whoopie Cushion"
 

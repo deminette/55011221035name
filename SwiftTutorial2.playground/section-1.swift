bd897240dd400f0b67f2094a9c4d1d50e481959c
55011212157Nay <student@tests-iMac-6.local>
2014-08-22 15:45:15 +0700

    Update SwiftTutorial

--- SwiftTutorial2.playground/section-1.swift
+++ SwiftTutorial2.playground/section-1.swift
@@ -14,17 +14,3 @@ let onsaleExplicit:Bool=true
 
 let nameInferred="whoopie Cushion"
 let nameExplicit:String="whoopie Cushion"
-
-var shoppingListExplicit=[String]()
-shoppingListExplicit = ["Egg","Milk"]
-//var shoppingListExplicit:[String]=["Eggs","Milk"]
-
-var shoppingList=["Eggs","Milk"]
-println("the shopping list contains \(shoppingList.count) items.")
-shoppingList.append("Flour")
-shoppingList+="Baking Powder"
-shoppingList+=["Chocolate Spread","Cheese","Butter"]
-var firstItem=shoppingList[0]
-shoppingList[0]="Six eggs"
-//shoppingList[0...3] = ["Bananas","Apples"]
-shoppingList
---
+++ SwiftTutorial5.playground/section-1.swift
@@ -0,0 +1,4 @@
+let array:Array<Int>=[1,2,3,4]
+let dictionary: Dictionary<String, Int> = ["dog": 1,"elephant" : 2]
+//let dictionary = ["dog": 1,"elephant" : 2]
+

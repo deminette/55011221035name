8a92a3cbe5fc3adda3ba77c207dea1b47ce0b635
54011212233Anuosrn <student@tests-iMac-46.local>
2014-08-29 17:11:25 +0700

    updt2
    2123

--- 6SwiftTutorial5Dictionary.playground/section-1.swift
+++
@@ -1,4 +0,0 @@
-let array:Array<Int>=[1,2,3,4]
-let dictionary: Dictionary<String, Int> = ["dog": 1,"elephant" : 2]
-//let dictionary = ["dog": 1,"elephant" : 2]
-
--- 9Lab2Zoo.playground/section-1.swift
+++
@@ -1,6 +0,0 @@
-var animal=["Tiger":"Meat","Utulmy":"Vegeteble","Blue":"Beer"]
-
-var user_data=["N01":"ac035","MSMS":"5500"]
-
-animal["Blue"]
-

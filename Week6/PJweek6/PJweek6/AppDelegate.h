8a3ff066751e421181f897fe88daeaca4d93dfc5
54011212233Anuosrn <student@tests-iMac-46.local>
2014-09-24 15:29:09 +0700

    Initial Commit

---
+++ Week6/PJweek6/PJweek6/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PJweek6
+//
+//  Created by student on 9/24/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

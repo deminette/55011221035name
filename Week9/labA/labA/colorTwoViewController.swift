8042a8bd7f297a3c710137dd7de9f46772380c65
supasin sriyotee <supasin-nik@hotmail.com>
2014-10-17 15:20:03 +0700

    Execubate
    asdasdasdasdads

---
+++ Week9/labA/labA/colorTwoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ColorTwoView.swift
+//  labA
+//
+//  Created by Student on 10/17/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+import UIKit
+
+class ColorTwoView: NSObject {
+   
+}
